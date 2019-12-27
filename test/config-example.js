45505077fa5fc89cc097253e4d6d0a7536079876
longjinwen <396983001@qq.com>
2019-12-27 16:29:12 +0800

    init

--- test/config-example.js
+++ test/config-example.js
@@ -1,4 +1,10 @@
 module.exports = {
+  host: 'http://x',
+  wsHost: 'ws://x',
+};
+
+
+module.exports = {
   host: 'http://39.108.132.50:8501',
   wsHost: 'ws://39.108.132.50:8501',
 };
--- test/export.js
+++ test/export.js
@@ -1,6 +1,5 @@
 module.exports = {
   Storm3: require('../project/storm3/export'),
-  // Web3: require('web3')
 };
 
 
